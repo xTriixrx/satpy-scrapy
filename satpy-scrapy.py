0f2d32aa43d70f2f72dfb4d60cbc137ba8a37a7d
Vincent Nigro <vnigro5743@gmail.com>
2021-02-11 21:41:07 -0500

    Added config schema & more secure tor-password presentation

--- satpy-scrapy.py
+++ satpy-scrapy.py
@@ -2,7 +2,9 @@ import sys
 import shlex
 import getopt
 import logging
+import hashlib
 import multitasking
+from xml.dom import minidom
 from crawlers.dscovr import DSCOVR
 from crawlers.goes_16 import GOES_EAST
 from crawlers.goes_17 import GOES_WEST
@@ -19,8 +21,16 @@ GOES-WEST (GOES-17), HIMAWARI-8, GEO-KOMPSAT-2A, FENGYUN-4A, METEOSAT-8, METEOSA
 
  @author Vincent Nigro
  @version 0.0.1
- @modified 2/10/21
+ @modified 2/11/21
 """
+
+ASCII = 'ascii'
+READ_ONLY = 'rb'
+TOR_ELEMENT = 'tor'
+PW_ATTRIBUTE = 'pw'
+SECRET_ELEMENT = 'secret'
+CONFIG_FILE_LOC = 'config.xml'
+LOCATION_ATTRIBUTE = 'location'
 
 def generate_utc_range_30_step(utcrange):
     """
@@ -97,7 +107,7 @@ def help_logger():
 
     print("Satellite Hi-Res IMG Scraper By Vincent Nigro")
     print("Version: 1.0.0")
-    print("Last Modified: 1/29/21")
+    print("Last Modified: 2/11/21")
     print("")
     print("This program extracts the latest high resolution images from various sources using the Tor Network.")
     print("")
@@ -105,28 +115,28 @@ def help_logger():
     print("\tpython3 satpy-scrapy.py --filters")
     print("")
     print("To extract all the latest GOES-EAST images")
-    print("\tpython3 satpy-scrapy.py -e --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -e")
     print("")
     print("To extract all the latest GOES-WEST images")
-    print("\tpython3 satpy-scrapy.py -w --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -w")
     print("")
     print("To extract all the latest HIMAWARI-8 images")
-    print("\tpython3 satpy-scrapy.py -i --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -i")
     print("")
     print("To extract all the latest DSCOVR images")
-    print("\tpython3 satpy-scrapy.py -d --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -d")
     print("")
     print("To extract the latest FENGYUN-4A image")
-    print("\tpython3 satpy-scrapy.py -f4a --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -f4a")
     print("")
     print("To extract the latest GK2A images")
-    print("\tpython3 satpy-scrapy.py -gk2a --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -gk2a")
     print("")
     print("To extract the latest METEOSAT-8 images")
-    print("\tpython3 satpy-scrapy.py -m8 --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -m8")
     print("")
     print("To extract the latest METEOSAT-11 images")
-    print("\tpython3 satpy-scrapy.py -m11 --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -m11")
     print("")
     print("To extract the latest ELEKTRO-L2 image")
     print("\tpython3 satpy-scrapy.py -k")
@@ -138,25 +148,25 @@ def help_logger():
     print("\tpython3 satpy-scrapy.py -k --day=\"25\" --utcrange=\"0000-2300\"")
     print("")
     print("To extract the latest FENGYUN-4A images within a UTC range")
-    print("\tpython3 satpy-scrapy.py -f4a --utcrange=\"0000-2300\" --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -f4a --utcrange=\"0000-2300\"")
     print("")
     print("To extract 'GeoColor' GOES-EAST image(s)")
-    print("\tpython3 satpy-scrapy.py -e --images=\"GeoColor\" --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -e --images=\"GeoColor\"")
     print("")
     print("To extract 'Derived Motion Winds' GOES-WEST image(s)")
-    print("\tpython3 satpy-scrapy.py -w --images=\"\\\"Derived Motion Winds\\\"\" --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -w --images=\"\\\"Derived Motion Winds\\\"\"")
     print("")
     print("To extract 'Enhanced Color' DSCOVR image(s)")
-    print("\tpython3 satpy-scrapy.py -d --images=\"\\\"Natural Color\\\"\" --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -d --images=\"\\\"Natural Color\\\"\"")
     print("")
     print("To extract 'GeoColor' and 'Derived Motion Winds' GOES-EAST images")
-    print("\tpython3 satpy-scrapy.py -e --images=\"GeoColor \\\"Derived Motion Winds\\\"\" --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -e --images=\"GeoColor \\\"Derived Motion Winds\\\"\"")
     print("")
     print("To extract 'Natural Color' and 'True Color' GK2A images")
-    print("\tpython3 satpy-scrapy.py -gk2a --images=\"\\\"Natural Color\\\" \\\"True Color\\\"\" --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -gk2a --images=\"\\\"Natural Color\\\" \\\"True Color\\\"\"")
     print("")
     print("To extract 'Natural Color' and 'True Color' HIMAWARI-8 images")
-    print("\tpython3 satpy-scrapy.py -i --images=\"\\\"Natural Color\\\" \\\"GeoColor\\\"\" --tor-password=\"password\"")
+    print("\tpython3 satpy-scrapy.py -i --images=\"\\\"Natural Color\\\" \\\"GeoColor\\\"\"")
     print("")
 
 
@@ -219,19 +229,18 @@ def handle_arguments(argv):
     Function which handles the command line arguments which had been presented during the start of the program.
 
     @param argv = [] - A list containing a set of arguments passed by the command line.
-    @return satellite, img_types, tor_pw: SatelliteCrawler, [], str
+    @return satellite, img_types: SatelliteCrawler, []
     """
 
     img_types = []
     utc_range = ''
     elektro_day = ''
-    tor_password = ''
     elektro_pass = False
     fengyun_4a_pass = False
     
     try:
         opts, args = getopt.getopt(argv, '-wehikdm:f:g:', 
-            ['help', 'filters', 'day=', 'utcrange=', 'images=', 'tor-password='])
+            ['help', 'filters', 'day=', 'utcrange=', 'images='])
         
         for opt, arg in opts:
             if opt == '-h' or opt == '--help':
@@ -247,15 +256,6 @@ def handle_arguments(argv):
                     fengyun_4a_pass = True
                 
         if not elektro_pass:
-            try:
-                tor_password = [arg for opt, arg in opts if opt == '--tor-password'][0]
-            except Exception as e:
-                tor_error = 'Required to submit a Tor password for access to configured ControlPort feature.'
-                tor_password = None
-                logging.error(tor_error)
-                print(tor_error)
-                sys.exit(1)
-            
             if fengyun_4a_pass:
                 try:
                     utc_range = [arg for opt, arg in opts if opt == '--utcrange'][0].split('-')
@@ -284,54 +284,109 @@ def handle_arguments(argv):
 
         for opt, arg in opts:
             if opt == '-d':
-                return DSCOVR(DSCOVR.DSCOVR_URL, DSCOVR.DSCOVR_NAME), img_types, tor_password
+                return DSCOVR(DSCOVR.DSCOVR_URL, DSCOVR.DSCOVR_NAME), img_types
             elif opt == '-w':
-                return GOES_WEST(GOES_WEST.GOES_WEST_URL, GOES_WEST.GOES_WEST_NAME, 10848), img_types, tor_password
+                return GOES_WEST(GOES_WEST.GOES_WEST_URL, GOES_WEST.GOES_WEST_NAME, 10848), img_types
             elif opt == '-e':
-                return GOES_EAST(GOES_EAST.GOES_EAST_URL, GOES_EAST.GOES_EAST_NAME, 10848), img_types, tor_password
+                return GOES_EAST(GOES_EAST.GOES_EAST_URL, GOES_EAST.GOES_EAST_NAME, 10848), img_types
             elif opt == '-i':
-                return HIMAWARI_8(HIMAWARI_8.HIMAWARI_8_URL, HIMAWARI_8.HIMAWARI_8_NAME), img_types, tor_password
+                return HIMAWARI_8(HIMAWARI_8.HIMAWARI_8_URL, HIMAWARI_8.HIMAWARI_8_NAME), img_types
             elif opt == '-k':
                 return ELEKTRO_L2(ELEKTRO_L2.ELEKTRO_L2_URL, ELEKTRO_L2.ELEKTRO_L2_NAME, elektro_day, 
-                    utc_range), img_types, tor_password
+                    utc_range), img_types
             elif opt == '-f':
                 if arg == '4a':
-                    return FENGYUN_4A(FENGYUN_4A.FENGYUN_4A_URL, FENGYUN_4A.FENGYUN_4A_NAME, utc_range), \
-                        img_types, tor_password
+                    return FENGYUN_4A(FENGYUN_4A.FENGYUN_4A_URL, FENGYUN_4A.FENGYUN_4A_NAME, utc_range), img_types
             elif opt == '-g':
                 if arg == 'k2a':
-                    return GEO_KOMPSAT_2A(GEO_KOMPSAT_2A.GEO_KOMPSAT_2A_URL, GEO_KOMPSAT_2A.GEO_KOMPSAT_2A_NAME), \
-                        img_types, tor_password
+                    return GEO_KOMPSAT_2A(GEO_KOMPSAT_2A.GEO_KOMPSAT_2A_URL, GEO_KOMPSAT_2A.GEO_KOMPSAT_2A_NAME), img_types
             elif opt == '-m':
                 if arg == '8':
-                    return METEOSAT_8(METEOSAT_8.METEOSAT_8_URL, METEOSAT_8.METEOSAT_8_NAME), \
-                        img_types, tor_password
+                    return METEOSAT_8(METEOSAT_8.METEOSAT_8_URL, METEOSAT_8.METEOSAT_8_NAME), img_types
                 elif arg == '11':
-                    return METEOSAT_11(METEOSAT_11.METEOSAT_11_URL, METEOSAT_11.METEOSAT_11_NAME), \
-                        img_types, tor_password
+                    return METEOSAT_11(METEOSAT_11.METEOSAT_11_URL, METEOSAT_11.METEOSAT_11_NAME), img_types
         
     except getopt.GetoptError as e:
         logging.exception(e)
         print(e)
         sys.exit(1)
 
-    return GOES_EAST(GOES_EAST.GOES_EAST_URL, GOES_EAST.GOES_EAST_NAME, 10848), img_types, tor_password
+    return GOES_EAST(GOES_EAST.GOES_EAST_URL, GOES_EAST.GOES_EAST_NAME, 10848), img_types
+
+
+def read_tor_secret():
+    """
+    This function will attempt to read a root-only accessible configuration file which contains an element named 'tor'
+    with an attribute containing 'pw' which holds the value of a password. This password is read in and is checked against
+    a hashed value stored in some secret file. This secret file is also root-only accessible meaning the entire satpy-scrapy
+    program requires root to start such a program in order to access the configuration and secret files. This is a safer method
+    than presenting the password at the command line where traffic sniffers can easily detect such a file.
+
+    @return tor_pw: str - A plaintext string representing the password configured for the Tor clients' ControlPort. 
+    """
+
+    tor_pw = ''
+    
+    try:
+        # Read & parse read only root configuration file
+        dom = minidom.parse(CONFIG_FILE_LOC)
+        tor_element = dom.getElementsByTagName(TOR_ELEMENT)
+        sec_element = dom.getElementsByTagName(SECRET_ELEMENT)
+
+        # Extract pw from secured configuration file and convert to binary
+        pw = tor_element[0].attributes[PW_ATTRIBUTE].value
+        pw = pw.encode(ASCII).rstrip()
+        
+        s_file = sec_element[0].attributes[LOCATION_ATTRIBUTE].value
+
+        # Get hashed store in secret hold file location
+        hw = ''
+        with open(s_file, READ_ONLY) as f:
+            hw = f.read()
+            hw = hw.decode(ASCII).rstrip()
+            f.close()
+        
+        hashed = hashlib.sha512(pw).hexdigest().rstrip()
+
+        if hashed == hw:
+            tor_pw = pw.decode(ASCII)
+        else:
+            err = 'Provided password in configuration file does not match secret hash.'
+            logging.error(err)
+            print(err)
+            sys.exit(1)
+
+    except Exception as e:
+        logging.error(e)
+        print(e)
+        sys.exit(1)
+    finally:
+        return tor_pw
 
 
 def main(argv):
     """
     Main program which executes the anonymous extraction of GOES-16, GOES-17, HIMAWARI-8,
-    FY-4A, GK2A, and ELEKTRO-L2 high resolution images. By default, the resolution for GOES vehicles is
-    set to the 10848x10848 resolution, FENGYUN-4A is typically just under 3k images, GK2A typically are under
-    1500x1500 and the HIMAWARI-8 images are either 11000x11000 or 5500x5500.
+    FY-4A, GK2A, METEOSAT-8, METEOSAT-11, DSCOVR, and ELEKTRO-L2 high resolution images. By default, 
+    the resolution for GOES vehicles is set to the 10848x10848 resolution, FENGYUN-4A is typically 
+    just under 3k images, GK2A typically are under 1500x1500 and the HIMAWARI-8 images are either 
+    11000x11000 or 5500x5500.
   
     To view configuration options, either run this program with the -h/--help flag or 
     view the help_logger() function above which describes examples and how to use this
     software.
     """
     
-    satellite, img_titles, tor_pw = handle_arguments(argv)
+    satellite, img_titles = handle_arguments(argv)
 
+    tor_pw = read_tor_secret()
+
+    if tor_pw == '':
+        err = 'No password was read in from the configuration file, exiting now.'
+        logging.error(err)
+        print(err)
+        sys.exit(1)
+    
     try:
         satellite.create_satellite_directory()
         

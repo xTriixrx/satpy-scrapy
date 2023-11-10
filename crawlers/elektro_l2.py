b6822bb49b69a93eda8d0b3a60a4c6ff1e46529c
xTriixrx <vnigro5743@gmail.com>
2023-11-10 12:15:08 -0700

    Enhanced Elektro-L2 crawler

--- crawlers/elektro_l2.py
+++ crawlers/elektro_l2.py
@@ -1,6 +1,7 @@
 import re
 import os
 import pytz
+from ftplib import FTP
 from datetime import datetime, timedelta
 from crawlers.satellite_crawler import SatelliteCrawler
 
@@ -13,15 +14,19 @@ class ELEKTRO_L2(SatelliteCrawler):
     FTP server.
 
     @author Vincent.Nigro
-    @version 0.0.1
-    @modified 1/27/21
+    @version 0.0.2
+    @modified 10/10/23
     """
     
     HALF_HOUR_MARK = 30
-    JPG_NAME = 'original_RGB.jpg'
+    ELEKTRO_L2_FTP_PORT = 2121
+    ELEKTRO_L2_USERNAME = 'electro'
+    ELEKTRO_L2_PASSWORD = 'electro'
     MOSCOW_TIME_ZONE = 'Europe/Moscow'
     ELEKTRO_L2_DIRECTORY = 'ELEKTRO-L2'
-    ELEKTRO_L2_URL = 'ftp://electro:electro@ntsomz.gptl.ru:2121/ELECTRO_L_2/'
+    ELEKTRO_L2_URL_HOSTNAME = 'ntsomz.gptl.ru'
+    ELEKTRO_L2_FTP_BASE_DIRECTORY = 'ELECTRO_L_2'
+    ELEKTRO_L2_URL = 'ftp://electro:electro@ntsomz.gptl.ru:2121/ELECTRO_L_2'
 
     def __init__(self, url, satellite, day, utcrange):
         """
@@ -36,7 +41,6 @@ class ELEKTRO_L2(SatelliteCrawler):
         self.__day = day
         self.__utcrange = utcrange
 
-
     def get_links(self, pw):
         """
         Implemented abstract public method which performs ELEKTRO-L2 specific processing for creating the appropriate
@@ -46,141 +50,159 @@ class ELEKTRO_L2(SatelliteCrawler):
         @return links: {} - A key-value mapping of a title key in a standard format and its appropriate image link.
         """
 
+        days = []
         links = {}
-        
-        if self.__utcrange != '' or self.__day != '':
-            links = self.__populate_ftp_link_range()
+
+        if self.__utcrange != "" and self.__day != "":
+            days.append(self.__day)
+            links = self.extract_links(utc_range=self.__utcrange, days=days)
+        elif self.__utcrange != "":
+            links = self.extract_links(utc_range=self.__utcrange)
+        elif self.__day != "":
+            days.append(self.__day)
+            links = self.extract_links(days)
         else:
-            # Get rounded utc time and full country time of russia
-            utctime, country_time = self.__get_russian_utc_time()
-            
-            # Extract each field required for building title, filename, and relative_path 
-            day, month, month_digits, year, utc = self.__get_date_fields(utctime)
-            
-            title, path = self.__generate_properties(day, month, month_digits, year, utc)
-            
-            self._logger.info(country_time.strftime("Current Date in Moscow, Russia is %d-%m-%y and the time is %H:%M:%S"))
-            
-            links[title] = self.get_url() + path
+            links = self.extract_links()
+
+        self._logger.debug("Generated links: ")
+        self._logger.debug(links)
 
         return links
-  
 
-    def __populate_ftp_link_range(self):
+    def extract_links(self, days=[], utc_range=[]):
         """
-        Private method which populates the links dictionary based on the configured properties set by
-        the constructor. Each setting will always generate the current Russian time properties and override
-        the necessary ones that need to be overwritten with command line options (ones passed by constructor).
+        Extracts and retrieves the file links for a set of days, within a utc_range. This will initiate
+        the FTP connection to the ELECTRO-L2 server and begin traversing the directories for the current
+        month.
 
+        @param days: [] - A list representing a single day; if empty it means retrieve all days.
+        @param utc_range: [] - A list of utc times in HHMM format to set the time range of returned links.
         @return links: {} - A key-value mapping of a title key in a standard format and its appropriate image link.
         """
 
         links = {}
 
-        # Get rounded utc time and full country time of russia
-        utctime, country_time = self.__get_russian_utc_time()
-        
-        self._logger.info(country_time.strftime("Current Date in Moscow, Russia is %d-%m-%y and the time is %H:%M:%S"))
+        # Get current utc time of russia
+        utctime = self.__get_russian_utc_time()
+        self._logger.debug("Approximate current russian utc time is: " + utctime.strftime("%x - %X"))
 
-        # Extract each field required for building title, filename, and relative_path 
-        day, month, month_digits, year, utc = self.__get_date_fields(utctime)
+        # Extract each field required for run
+        year, month = self.__get_date_fields(utctime)
+        self._logger.debug("Current year: " + year)
+        self._logger.debug("Current month: " + month)
 
-        if self.__utcrange != '' and self.__day != '':
-            for utc in self.__utcrange:
-                title, path = self.__generate_properties(self.__day, month, month_digits, year, utc)
-                links[title] = self.get_url() + path
-        elif self.__utcrange != '':
-            for utc in self.__utcrange:
-                title, path = self.__generate_properties(day, month, month_digits, year, utc)
-                links[title] = self.get_url() + path
-        elif self.__day != '':
-            title, path = self.__generate_properties(self.__day, month, month_digits, year, utc)
-            links[title] = self.get_url() + path
-        
+        # Initiate FTP connection
+        with FTP() as ftp:
+            ftp.connect(self.ELEKTRO_L2_URL_HOSTNAME, self.ELEKTRO_L2_FTP_PORT)
+            self._logger.debug("Connecting to FTP server at: " + self.ELEKTRO_L2_URL_HOSTNAME \
+                               + ":" + str(self.ELEKTRO_L2_FTP_PORT) + ".")
+
+            ftp.login(self.ELEKTRO_L2_USERNAME, self.ELEKTRO_L2_PASSWORD)
+            self._logger.debug("Logging into FTP server with credentials: " + self.ELEKTRO_L2_USERNAME \
+                               + ":" + self.ELEKTRO_L2_PASSWORD + ".")
+            base_dir = self.ELEKTRO_L2_FTP_BASE_DIRECTORY + '/' + year + '/' + month
+            ftp.cwd(base_dir)
+            self._logger.debug("Changed directory to: " + base_dir + ".")
+
+            # If days is empty, get the list of days in the current directory
+            if not days:
+                days = ftp.nlst()
+
+            # Iterate over each day and get the links for each day, aggregating them into the links map
+            for day in days:
+                tmp_links = self.extract_links_for_day(ftp, year, month, day, utc_range)
+                self._logger.debug("Changed directory back to: " + base_dir + ".")
+                links = {**tmp_links, **links}
+        self._logger.debug("Closing connection to FTP server at: " + self.ELEKTRO_L2_URL_HOSTNAME \
+                           + ":" + str(self.ELEKTRO_L2_FTP_PORT) + ".")
+
         return links
 
-
-    def __generate_properties(self, day, month, month_digits, year, utc):
+    def extract_links_for_day(self, ftp, year, month, day, utc_range=[]):
         """
-        A private method which returns the title and sub-path to the image located on the FTP hostname.
+        Extracts and retrieves the file links for a given day within a utc_range. If the utc_range
+        is provided, each utc_time will be compared to determine whether the time is within the range.
+        Otherwise, all utc_times will be retrieved with all of the files associated in the share.
 
-        @param day: str - A string digit representing the day ex: 25.
-        @param month: str - A capitalized month ex: January.
-        @param month_digits: str - A string containing 2 digits representing the month ex: 01.
-        @param year: str - A year formated as YYYY ex: 2021.
-        @param utc: str - A UTC formatted as XXXX ex: 0030 for 00:30 UTC.
-        @return title, path: str, str - The standardized title and relative path.
-        """
-
-        # Original RGB - 26 Jan 2021 - 03-30 UTC
-        title = self.__generate_title(day, month, year, utc)
-        
-        #YYMMDD_UTC_original_RGB.jpg
-        filename = self.__generate_filename(day, month_digits, year, utc)
-
-        # YYY/Jan/DD/UTC/img_name
-        path = self.__generate_path(day, month, year, utc, filename)
-
-        return title, path
-
-
-    def __generate_title(self, day, month, year, utc):
-        """
-        A private method which generates a standardized title format to be used as a directory to contain the
-        linked image as well as other processing done by the standard pulldown procedure.
-
-        @param day: str - A string digit representing the day ex: 25.
-        @param month: str - A capitalized month ex: January.
-        @param year: str - A year formated as YYYY ex: 2021.
-        @param utc: str - A UTC formatted as XXXX ex: 0030 for 00:30 UTC.
-        @return title: str - A title with a standardized format such as: Original RGB - 26 Jan 2021 - 03-30 UTC
+        @param ftp: FTP - An FTP object to traverse the ELECTRO_L_2 FTP share.
+        @param year: str - The current year.
+        @param month: str - A string representing the current month.
+        @param day: str - A string representing some day of the month.
+        @param utc_range: [] - A list of utc times in HHMM format to set the time range of returned links.
+        @return links: {} - A key-value mapping of a title key in a standard format and its appropriate image link.
         """
 
-        title = 'Original RGB - ' + day + ' ' + month[:3] + ' ' + \
-                year + ' - ' + utc[:2] + '-' + utc[-2:] + ' ' + self.UTC_STRING
-        
-        return title
+        links = {}
 
+        # Enter the valid day directory and get the list of all the utc_times available
+        ftp.cwd(day)
+        self._logger.debug("Changed directory to: " + day + ".")
+        utc_times = ftp.nlst()
 
-    def __generate_path(self, day, month, year, utc, filename):
-        """
-        A private method which generates a relative path on the FTP server to be concatenated 
-        to the base url hostname.
-        
-        @param day: str - A string digit representing the day ex: 25.
-        @param month: str - A capitalized month ex: January.
-        @param year: str - A year formated as YYYY ex: 2021.
-        @param utc: str - A UTC formatted as XXXX ex: 0030 for 00:30 UTC.
-        @param filename: str - The filename of current link gen iteration with format: YYMMDD_UTC_original_RGB.jpg
-        @return path: str - A relative path on the FTP server to be concatenated with base url hostname.
-        """
+        # For each utc_time if its within range get the files & create links
+        for utc_time in utc_times:
+            files = []
 
-        if int(day) <= 9:
-            path = year + '/' + month + '/' + '0' + day + '/' + utc + '/' + filename
-        else:
-            path = year + '/' + month + '/' + day + '/' + utc + '/' + filename
+            # If the current time is not within the utc_range, skip over it
+            if utc_time not in utc_range and utc_range:
+                self._logger.debug(
+                    "UTC time " + utc_time[0:2] + "-" + utc_time[2:4] + " UTC is not within provided UTC range.")
+                continue
 
-        return path
+            self._logger.debug("Generating titles and links for files captured for UTC Time: " + utc_time)
 
+            # Enter the valid utc directory and get the list of all the files
+            ftp.cwd(utc_time)
+            self._logger.debug("Changed directory to: " + utc_time + ".")
 
-    def __generate_filename(self, day, month_digits, year, utc):
-        """
-        A private method which returns the filename being queried for the current iteration of link generation.
+            files = ftp.nlst()
 
-        @param day: str - A string digit representing the day ex: 25.
-        @param month_digits: str - A string containing 2 digits representing the month ex: 01.
-        @param year: str - A year formated as YYYY ex: 2021.
-        @param utc: str - A UTC formatted as XXXX ex: 0030 for 00:30 UTC.
-        @return filename: str - The filename of current link gen iteration with format: #YYMMDD_UTC_original_RGB.jpg
-        """
+            # For each file, generate the title and link & insert into links map
+            for file in files:
+                # Skip over any zip files
+                if file.endswith('.zip'):
+                    continue
 
-        if int(day) <= 9:
-            filename = year[-2:] + month_digits + '0' + day + '_' + utc + '_' + self.JPG_NAME
-        else:
-            filename = year[-2:] + month_digits + day + '_' + utc + '_' + self.JPG_NAME
+                self._logger.debug("Generating title and link for file: " + file)
 
-        return filename
+                # Get time from file and parse into datetime for better readability
+                time = file.split('_')
+                time = time[0] + time[1]
+                self._logger.debug("Captured time value from file name: " + time)
 
+                # Insert captured string time into datetime object with associated time format
+                date = datetime.strptime(time, '%y%m%d%H%M').date()
+
+                # Get image type in file name
+                img_type = file.split('.')[0]
+                img_type = img_type.split('_')[2:]
+                img_type = "_".join(img_type)
+                img_type = img_type.lower().replace("_", " ").title()
+
+                if img_type.isdigit():
+                    img_type = "Band " + img_type
+
+                self._logger.debug("Generated image type of: " + img_type)
+
+                # Create title for given file
+                title = img_type + ' - ' + date.strftime('%Y-%m-%d') + \
+                        ' - ' + utc_time[0:2] + '-' + utc_time[2:4] + ' UTC'
+
+                # Create link for file
+                link = (self.ELEKTRO_L2_URL + '/' + year + '/' + month + '/' + day + \
+                        '/' + utc_time + '/' + file)
+
+                self._logger.debug("Scraped link " + link + " for image " + title + ".")
+
+                # Insert title and link into links map
+                links[title] = link
+            # Navigate back one directory
+            ftp.cwd('..')
+            self._logger.debug("Changed directory back to: " + day + ".")
+        # Navigate back one directory
+        ftp.cwd('..')
+
+        return links
 
     def __get_russian_utc_time(self):
         """
@@ -198,19 +220,18 @@ class ELEKTRO_L2(SatelliteCrawler):
         utctime = country_time.utcnow()
         
         # Russia operates +3 hours ahead of UTC time.
-        difference = timedelta(hours = 3)
+        difference = timedelta(hours=3)
         utctime += difference
         
         # FTP server pushes files every half hour in UTC time.
         if utctime.minute > self.HALF_HOUR_MARK:
-            difference = timedelta(minutes = (utctime.minute - self.HALF_HOUR_MARK))
+            difference = timedelta(minutes=(utctime.minute - self.HALF_HOUR_MARK))
             utctime -= difference
         else:
-            difference = timedelta(minutes = utctime.minute)
+            difference = timedelta(minutes=utctime.minute)
             utctime -= difference
         
-        return utctime, country_time
-
+        return utctime
 
     def __get_date_fields(self, utctime):
         """
@@ -218,17 +239,13 @@ class ELEKTRO_L2(SatelliteCrawler):
         link path and standardized title.
 
         @param utctime: datetime - A datetime object containing the current russian UTC time.
-        @return day, month, month_digits, year, utc
+        @return year, month: str - Strings representing the year and month respectively.
         """
 
-        year = utctime.strftime('%Y') # YYYY
-        month = utctime.strftime('%B') # January
-        month_digits = utctime.strftime('%m') # 01
-        day = utctime.strftime('%d') # 26
-        utc = utctime.strftime('%H') + utctime.strftime('%M') # 0330 -> 03:30 UTC
+        year = utctime.strftime('%Y')  # YYYY
+        month = utctime.strftime('%B')  # January
 
-        return day, month, month_digits, year, utc
-
+        return year, month
 
     def _create_img_dir(self, title):
         """
@@ -252,7 +269,6 @@ class ELEKTRO_L2(SatelliteCrawler):
         self._logger.debug("Image directory path for title " + title + ": " + dir_path + ".")
 
         return dir_path
-
 
     def create_satellite_directory(self):
         """
